5ef690de54c0c555aa783e650d4f702bcf2613a0
UseCodeJqy <53791251+UseCodeJqy@users.noreply.github.com>
2021-06-07 17:56:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const TOKEN = 'ODQ5MzYzMDE5NDA5MzkxNjY4.YLaE9A.3tFa5Nm61Zj4fq6Go_5f0BJ3dcQ'
 const prefix = '!'
 
 bot.on('ready', () => {
-    console.log('FairShop ist nun Online!')
+    console.log('Der Bot ist nun Online!')
 
     bot.user.setPresence({
         activity: {
@@ -20,6 +20,21 @@ bot.on('message', message => {
     if(parts[0] == '!help') {
         message.channel.send('**Hier meine Befehle**\n**!clear**/**!purge** - Löscht bis zu 100 Nachrichten\n**!member** - Sagt dir, wieviele Mitglieder der Server hat, auf dem du dich befindest.\n**!owner** - Sagt dir, wer der die Eigentumsrechte von einem Server hat.\n**!userinfo <@>** - Damit kannst du dir die Benutzerinfo von dir oder jmd anderes anzeigen lassen')
     }
+    else if(parts[0] == '!clear' || parts[0] == '!purge') {
+        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Du brauchst die Berechtigung, Nachrichten zu löschen!')
+        if(!parts[1]) return message.channel.send('Du musst angeben, wieviele Nachrichten du löschen möchtest!')
+        if(isNaN(parts[1])) return message.channel.send('Die Angabe, wieviele Nachrichten du löschen möchtest, muss eine Zahl sein!')
+        if(parts[1] > 100) return message.channel.send('Du kannst nicht mehr als 100 Nachrichten löschen!')
+        if(parts[1] < 2) return message.channel.send('Du kannst nicht weniger als 2 Nachricht löschen')
+        message.channel.bulkDelete(parts[1])
+        message.channel.send(`Ich habe erfolgreich **${parts[1]}** Nachrichten gelöscht!`).then(m => m.delete({timeout: 3000}))
+    }
+    else if(parts[0] == '!member') {
+        message.channel.send(`Derzeit sind auf **${message.guild.name}** **${message.guild.members.cache.filter(m => m.user.bot).size}** Mitglieder online!`)
+    }
+    else if(parts[0] == '!owner') {
+        message.channel.send(`Der Owner von **${message.guild.name}**-Server ist **${message.guild.owner.user.tag}**`)
+    }
     else if(parts[0] == '!userinfo') {
 
         const guild = message.guild
@@ -28,7 +43,7 @@ bot.on('message', message => {
 
         const userr = member.user
 
-        const newEmbed = new Discord.MessageEmbed()
+        const embed = new Discord.MessageEmbed()
         .setColor('69e3e2')
         .setAuthor(`${usr.tag}`, `${usr.displayAvatarURL({dynamic: true})}`)
         .setThumbnail(`${usr.displayAvatarURL({dynamic: true})}`)
@@ -46,9 +61,9 @@ bot.on('message', message => {
             value: member.roles.cache.size - 1,
         })
 
-        message.channel.send(newEmbed)
+        message.channel.send(embed)
     }
-    else if(message.content.includes('@BOTID')) {
+    else if(message.content.includes('<@!BOTID>')) {
         const embed = new Discord.MessageEmbed()
         .setColor('ff0000')
         .setTitle('**Was gibts?**')
