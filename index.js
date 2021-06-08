6777cd3bf6ca3305a71ac8d0318a275238a55f49
UseCodeJqy <53791251+UseCodeJqy@users.noreply.github.com>
2021-06-08 17:38:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,6 @@ bot.on('ready', () => {
     })
 })
 
-
-bot.on('guildMemberAdd', guildMember =>{
-    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'Käufer');
-
-    guildMember.roles.add(welcomeRole)
-});
-
 bot.on('message', message => {
     let parts = message.content.split(" ");
 
@@ -28,7 +21,7 @@ bot.on('message', message => {
     if(parts[0] == '!help') {
         message.channel.send('**Hier meine Befehle**\n**!clear**/**!purge** - Löscht bis zu 100 Nachrichten\n**!member** - Sagt dir, wieviele Mitglieder der Server hat, auf dem du dich befindest.\n**!owner** - Sagt dir, wer der die Eigentumsrechte von einem Server hat.\n**!userinfo <@>** - Damit kannst du dir die Benutzerinfo von dir oder jmd anderes anzeigen lassen')
     }
-    else if(parts[1] == '!clear' || parts[0] == '!purge') {
+    else if(parts[0] == '!clear' || parts[0] == '!purge') {
         if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Du brauchst die Berechtigung, Nachrichten zu löschen!')
         if(!parts[1]) return message.channel.send('Du musst angeben, wieviele Nachrichten du löschen möchtest!')
         if(isNaN(parts[1])) return message.channel.send('Die Angabe, wieviele Nachrichten du löschen möchtest, muss eine Zahl sein!')
@@ -36,9 +29,6 @@ bot.on('message', message => {
         if(parts[1] < 1) return message.channel.send('Du kannst nicht weniger als 1 Nachricht löschen')
         message.channel.bulkDelete(parts[1])
         message.channel.send(`Ich habe erfolgreich **${parts[1]}** Nachrichten gelöscht!`).then(m => m.delete({timeout: 3000}))
-    }
-    else if(parts[0] == '!member') {
-        message.channel.send(`Derzeit sind auf **${message.guild.name}** **${message.guild.members.cache.filter(m => m.user.bot).size}** Mitglieder online!`)
     }
     else if(parts[0] == '!owner') {
         message.channel.send(`Der Owner von **${message.guild.name}**-Server ist **${message.guild.owner.user.tag}**`)
